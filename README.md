64eb98d3054c8c89f55274d1c0a36d93f3c70b12
Saleh Kalouti <salehkalouti@gmail.com>
2022-01-03 10:02:52 +0200

    changes by saleh 03/01/2022 10:02AM

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # GOT-giphy-finder
+
+got giphy finder is yur very own simple web application to get gifs for your favourite Game of Thrones character.
+all you have to do is type in a name or select one from a list of goats and get you GIF.
+
+a goal : the goal of this project is to give a easy user friendly way to get your char. even if you firget their name
+
+functionality: type or select from a long list of names to get your GIF that simple , you have the option of choosing up to 5 GIFs
+
+challenges : at the very first step we faced a problem with the API, we wanted a search depending on a char. name and the api search was with char. ID so after a lot of research we found an API that will provide us with good number of char. (53 to be precise), and that API does not work all the time so we we needed to find a backup if the API fails.
+
+another challenge was merging between a dropdown-list and an input-->type=text which eventually we found a way to do.
+
+technologies:
+-Visual Studio Code for code and text editing
+-languges used : HTML, JavaScript, CSS
+-git
+-Google Chrome for resources
+-GitHub for resources
+
+authors : Saleh Kalouti, Raneen, Asra Ayoub
+mentors : Mahmoud Diab
+
+WebAhead-13
--- index.js
+++ index.js
@@ -1,42 +1,68 @@
+var charArr = [];
 window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
-  
 
-fetch(`https://thronesapi.com/api/v2/Characters`)
+
+    fetch(`https://thronesapi.com/api/v2/Characters`)
     .then(response => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
     .then(json => 
         {
-        let parent = document.querySelector("datalist")
-        // let charArr = [];
+        var parent = document.querySelector("datalist")
+        
         json.forEach(element => {
-            // charArr.push(element.fullName)
+          charArr.push(element.fullName)
+            
+            console.log(element.fullName);
             const x = document.createElement("option")
             x.textContent = element.fullName
             parent.appendChild(x)
-        });
-        // console.log(charArr);
+        })
+        
+      })
+      .catch(error => console.error(error));
+    });
 
-    })
-    .catch(error => console.error(error));
-});
 
+// for(let i =1; i<1136;i++){
+// fetch(`https://anapioficeandfire.com/api/characters/${i}`)
+//     .then(response => {
+//       if (!response.ok) throw new Error(response.status);
+//       return response.json();
+//     })
+//     .then(json => 
+//         {
+//         var parent = document.querySelector("datalist")
+//               if(json.name!==""){
+//             charArr.push(json.name)}
+          
+            
+//             const x = document.createElement("option")
+//             x.textContent = json.name
+//             parent.appendChild(x)
+        
+
+//     })
+//     .catch(error => console.error(error));
+    
+// }
+// });
+
+console.log(charArr);
 
 const form = document.querySelector("form");
 let APIKEY = "bFxMie1ZPZn4Cfo76DER6CTzbJjb4rcc";
+
 
 form.addEventListener("submit", event => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
   const ch = formData.get("value");
-  const ch_ = ch.toLowerCase().trim()
   const limit = formData.get("limit")
-  console.log(limit);
-  console.log(ch_);
-  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=${limit}&q=${ch_}`)
+  const fetchGif = (ch_, limit, APIKEY) => fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=${limit}&q=${ch_}`)
     .then(response => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
@@ -53,6 +79,39 @@ form.addEventListener("submit", event => {
             
       
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+      const output = document.querySelector("output");
+      const error_msg = document.createElement("h2");
+      error_msg.innerHTML = "we have no GIF for that";
+
+
+      output.appendChild(error_msg)
+    });
+  
+  if(charArr.includes(ch)){
+  const ch_ = ch.toLowerCase().trim()
+  fetchGif(ch_,limit,APIKEY)
+  }else
+  {
+    // console.log(cahrArr[0]);
+    // console.log(ch);
+    // console.log(ch===charArr[0]);
+    const output = document.querySelector("output");
+    output.innerHTML = ""
+
+    let errMes = document.createElement("span")
+    errMes.textContent = "That is not a GOT character"
+
+    let img__ = document.createElement("img")
+    img__.src = "./not_a_got.jpg"
+      
+    output.appendChild(errMes)
+    output.appendChild(img__)
+  }
+  
+  
+  // console.log(limit);
+  // console.log(ch_);
+  
 });
 
